dacff5b0dd95c11bedab18311997464a2ebecef4
JoaoTrajano <joao.trajanosouza@gmail.com>
2023-04-19 10:38:09 -0300

    feito o cabeçalho

---
+++ src/components/Home/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import logo from '../../assets/logo.png';
+import search from '../../assets/search.png'
+import  './style.css'
+
+function Home() {
+  return (
+    <header>
+      <section className='cabecalho'>
+        <div>
+          <img src={logo} alt="logo" />
+        </div>
+        <div className='container'>
+          <input className='input' placeholder='O você procura?' type="text" />
+          <img src={search} alt="search" />
+        </div>
+      </section>
+    </header>
+  )
+}
+
+export default Home;
